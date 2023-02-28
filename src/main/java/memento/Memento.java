818e2b4309f79b72848083d2c334c61c3fe20399
JMateoVelascoAuza <mateovelascop19@gmail.com>
2023-02-28 18:00:17 -0400

    Practica Memento

---
+++ src/main/java/memento/Memento.java
@@ -0,0 +1,13 @@
+package memento;
+
+public class Memento {
+    private Backup state;
+
+    public Memento(Backup stateSaved){
+        this.state=stateSaved;
+    }
+
+    public Backup getState() {
+        return state;
+    }
+}
